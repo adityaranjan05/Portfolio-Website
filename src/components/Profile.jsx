e489a3510ccb1c0b922f80a1d0342bf9c9e5db62
Aditya Ranjan Sinha <aditya.ranjan.cs@gmail.com>
2026-06-26 19:33:41 +0530

    Removed padding from the profile pic

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import profilepic from "../assets/profile2.png";
 
 function Profile() {
     return (
-        <img src={profilepic} className='w-75 h-75 lg:w-125 lg:h-125 rounded-full mx-auto my-auto border-2 duration-250 hover:border-orange-600 hover:shadow-2xl hover:shadow-orange-600/70 hover:scale-105 p-4'></img>
+        <img src={profilepic} className='w-75 h-75 lg:w-125 lg:h-125 rounded-full mx-auto my-auto border-2 duration-250 hover:border-orange-600 hover:shadow-2xl hover:shadow-orange-600/70 hover:scale-105'></img>
     )
 }
 
